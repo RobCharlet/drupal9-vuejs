4b0af5b0f1ebd551967bbaf8886bbdda7df41cd3
Robin Charlet <robin.charlet@laposte.net>
2020-12-14 11:15:01 +0100

    Create event listener example

---
+++ modules/custom/module_hero/src/EventSubscriber/HeroInitSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Drupal\module_hero\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Our Event Subscriber
+ */
+class HeroInitSubscriber implements EventSubscriberInterface
+{
+  public function __construct()
+  {
+  }
+
+  public function onRequest($event) {
+    var_dump("hello from our event");
+  }
+
+  public static function getSubscribedEvents()
+  {
+    $events[KernelEvents::REQUEST][] = ['onRequest'];
+    return $events;
+  }
+}
